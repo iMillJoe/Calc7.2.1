eb594a318a3622584f2470e42bdb41bad96040e6
iMillJoe <josephmillion@me.com>
2014-06-15 18:56:05 -0500

    Added GNU license

--- Calc7.2/IMCircumcircleViewController.h
+++ Calc7.2/IMCircumcircleViewController.h
@@ -2,8 +2,24 @@
 //  IMCircumcircleViewController.h
 //  Calc7.2
 //
-//  Created by Joe Million on 3/22/14.
-//  Copyright (c) 2014 iMillIndustries. All rights reserved.
+//  Created by iMill Industries on 3/22/14.
+/*
+ < SOHCOATOA, an app for working with triangles >
+ Copyright (C) <2014>  <iMill Industries>
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program. (see IMAppDelegate.h) If not, see <http://www.gnu.org/licenses/>.
+ *///
 //
 
 #import <UIKit/UIKit.h>
